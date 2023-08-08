d58bdfc4b6f25c0dd7e1ae003ec18802f7c8c05d
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-08-08 22:25:15 +0300

    First work prototype.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,14 +1,24 @@
 
 #include <iofox.hpp>
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/address.hpp>
 #include <boost/asio/co_spawn.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <packio/json_rpc/rpc.h>
+#include <packio/client.h>
 #include <fmt/core.h>
 #include <exception>
 
 namespace asio { using namespace boost::asio; }
 
-auto coro() -> io::coro<void>
+auto coro(asio::io_context & executor) -> io::coro<void>
 {
+	asio::ip::tcp::socket socket {executor};
+	co_await socket.async_connect({asio::ip::make_address("127.0.0.1"), 555}, io::use_coro);
+
+	auto client = packio::make_client<packio::json_rpc::rpc>(std::move(socket));
+	co_await client->async_call("some_foo", io::use_coro);
+
 	fmt::print("sis.\n");
 	co_return;
 }
@@ -17,7 +27,7 @@ int main() try
 {
 	io::windows::set_asio_locale(io::windows::lang::english);
 	asio::io_context ctx;
-	asio::co_spawn(ctx, coro(), io::rethrowed);
+	asio::co_spawn(ctx, coro(ctx), io::rethrowed);
 	ctx.run();
 	return 0;
 }
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,23 +1,33 @@
 
 #include <iofox.hpp>
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/address.hpp>
 #include <boost/asio/co_spawn.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <packio/json_rpc/rpc.h>
+#include <packio/dispatcher.h>
+#include <packio/server.h>
 #include <fmt/core.h>
 #include <exception>
 
 namespace asio { using namespace boost::asio; }
 
-auto coro() -> io::coro<void>
-{
-	fmt::print("sas.\n");
-	co_return;
-}
-
 int main() try
 {
 	io::windows::set_asio_locale(io::windows::lang::english);
 	asio::io_context ctx;
-	asio::co_spawn(ctx, coro(), io::rethrowed);
+
+	asio::ip::tcp::acceptor acceptor {ctx, {asio::ip::make_address("127.0.0.1"), 555}};
+	auto server = packio::make_server<packio::json_rpc::rpc>(std::move(acceptor));
+
+	server->dispatcher()->add_coro("some_foo", ctx, [] -> io::coro<void>
+	{
+		fmt::print("message");
+		co_return;
+	});
+
+	server->async_serve_forever();
+
 	ctx.run();
 	return 0;
 }
